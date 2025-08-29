5dda8ec606f8d2bf5b1140cfd8eb22b4a9f3d8b1
rakshithagr <grrakshita1820@gmail.com>
2025-08-29 14:50:12 +0530

    upload commit

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -65,4 +65,3 @@ const Header = () => {
 
 export default Header;
 
-
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 //json
 import navbar from "@/components/Assets/Data/navbar.json";
 import { IoMdClose } from "react-icons/io";
 import Link from "next/link";
 
 const Sidebar = ({ setSidebar }) => {
+  const [activeLink, setActiveLink] = useState("null")
   return (
     <section className=" fixed top-0 left-0 w-[100%] h-[100%] z-10000 bg-[#04091e] bg-opacity[/100 border">
       <section className=" w-[100%] h-[100%] mt-0 mr-0 mb-0 ml-auto bg-[var(--light-bg)] ">
@@ -18,33 +19,18 @@ const Sidebar = ({ setSidebar }) => {
             <Link
               key={item.id}
               href={item.link}
-              className=" py-10 border-b border-[var(--primary)] text-lg  flex justify-center  items-center  hover:text-[--white]  text-[var(--white)] "
+              onClick={() => {
+                setActiveLink(item.link)
+                setSidebar(false)
+              }}
+              className= {`py-10 border-b border-[var(--primary)] text-lg  flex justify-center  items-center  hover:text-[--white]  text-[var(--white)]  ${
+                activeLink === item.link ? "" : ""
+              } `}
             >
               {item.title}
             </Link>
           ))}
-          {/* <section className="h-1/7 flex justify-center items-center gap-4   text-2xl text-[var(--primary)]">
-            <div>
-              <Link href="https://www.facebook.com/sabkaloan2024/">
-                <FaFacebookSquare />
-              </Link>
-            </div>
-            <div>
-              <Link href="https://www.instagram.com/sabka_loan/?hl=en">
-                <AiFillInstagram />
-              </Link>
-            </div>
-            <div>
-              <Link href="https://www.linkedin.com/company/sablka-loan/?viewAsMember=true">
-                <FaLinkedin />
-              </Link>
-            </div>
-            <div className="text-4xl text-[var(--primary)]">
-              <Link href="https://www.youtube.com/@Sabkaloan/videos">
-                <FaYoutube />
-              </Link>
-            </div>
-          </section> */}
+         
         </nav>
       </section>
     </section>
